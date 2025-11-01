fdb1f61ade3e56e353fbacc8dcb28db88f4afb76
Ga Min Cha <prettygamin@naver.com>
2025-11-02 00:27:54 +0900

    Add REST api

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from rest_framework import viewsets
+from .serializers import PostSerializer
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -39,3 +41,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+class blogImage(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
